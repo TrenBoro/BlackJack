5dad59c8b3bf9864359498c4e0d01df290ade4d1
TrenBoro <126170033+TrenBoro@users.noreply.github.com>
2023-02-23 14:05:33 +0100

    Add files via upload

---
+++ Black_Jack.py
@@ -0,0 +1,136 @@
+from os import sep
+from Black_Jack_Classes import Card, Deck, Hand, Chips, Game
+
+def take_bet(chips):
+    while True:
+        try:
+            chips.bet = int(input("\nHow many chips are you betting? > "))
+        except ValueError:
+            print('\nPlease enter an integer!')
+        else:
+            if chips.bet > chips.total:
+                print("\nYou don't have that many chips!")
+            else:
+                break
+
+def hit(deck, hand):
+    hand.add_card(deck.deal_one())
+
+def hit_or_stand(deck, hand, gamestate):
+    while True:
+        choice = input("\nHIT or to STAND? > ").lower()
+        if choice == "hit":
+            hit(deck, hand)
+        elif choice == 'stand':
+            gamestate.set_playing(False)
+            print("\nThe dealer is playing")
+        else:
+            print("\nPlease enter <hit> or <stand> ")
+            continue
+        break
+
+def show_some(player, dealer):
+    print("\nDealer's hand:")
+    print(f"HIDDEN\n{dealer.deck[1]}")
+    print("\nPlayer's hand:")
+    print(*player.deck, sep = "\n")
+    print(f"\nPlayer is at a total of {player.value}")
+
+def show_all(player, dealer):
+    print("\nDealer's hand:")
+    print(*dealer.deck, sep = "\n")
+    print(f"\nDealer is at a total of {dealer.value}")
+    print("\nPlayer's hand:")
+    print(*player.deck, sep = "\n")
+    print(f"\nPlayer is at a total of {player.value}")
+
+def player_bust(chips):
+    print("\nPlayer busts!")
+    chips.lose_bet()
+
+def dealer_bust(chips):
+    print("\nDealer busts!")
+    chips.win_bet()
+
+def player_loss(chips):
+    print("\nPlayer loses!")
+    chips.lose_bet()
+
+def dealer_loss(chips):
+    print("\nDealer loses!")
+    chips.win_bet()
+
+def push():
+    print("\nPlayer and Dealer are at a tie. It's a push!!!!")
+
+def main():
+
+    player_chips = Chips() # So that we don't lose chips between rounds
+
+    while True:
+        print("\tWELCOME TO BLACKJACK!!!\n\n")
+
+        game = Game()
+
+        deck = Deck()
+        deck.shuffle()
+
+        player_hand = Hand()
+        player_hand.add_card(deck.deal_one())
+        player_hand.add_card(deck.deal_one())
+
+        dealer_hand = Hand(True)
+        dealer_hand.add_card(deck.deal_one())
+        dealer_hand.add_card(deck.deal_one())
+
+        print("\nThe player and dealer have been dealt the cards!")
+        
+        show_some(player_hand, dealer_hand)
+
+        print(f"You have {player_chips.total} chips!")
+
+        take_bet(player_chips)
+
+        while game.get_playing():
+            hit_or_stand(deck, player_hand, game)
+
+            show_some(player_hand, dealer_hand)
+
+            if player_hand.value > 21:
+                player_bust(player_chips)
+                break # break out of playing
+
+        if player_hand.value <= 21:
+            while dealer_hand.value < 17:
+                hit(deck, dealer_hand)
+
+            show_all(player_hand, dealer_hand)
+            
+            if dealer_hand.value > 21:
+                dealer_bust(player_chips)
+            elif dealer_hand.value > player_hand.value:
+                player_loss(player_chips)
+            elif dealer_hand.value < player_hand.value:
+                dealer_loss(player_chips)
+            else:
+                push()
+
+        print(f"Game ended with {player_chips.total}\n\n")
+
+        again = input("\tWould you like to play again? y/n > ").lower()
+
+        if player_chips.total < 0:
+            print("\nYou don't have any chips left...")
+            break
+        exit()
+
+        else:
+            if again == 'y':
+                continue
+    
+            else:
+                print("That's not a no, but whatever. Bye!")
+                break
+
+if __name__ == '__main__':
+    main()
---
+++ Black_Jack_Classes.py
@@ -0,0 +1,95 @@
+import random
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
+         'Queen':10, 'King':10, 'Ace':11}
+
+playing = True
+
+class Game():
+
+    def __init__(self):
+        pass
+
+    def get_playing(self):
+        return playing
+
+    def set_playing(self, state):
+        global playing
+        playing = state
+
+class Card():
+    
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+        self.value = values[rank]
+
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
+
+class Deck():
+
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(rank, suit))
+
+    def __str__(self):
+        deck_str = ''
+        for card in self.deck:
+            deck_str += card.__str__() + "\n"
+
+        return "The deck consists of: \n" + deck_str
+    
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal_one(self):
+        return self.deck.pop()
+
+class Hand():
+
+    def __init__(self, dealer = False):
+        self.deck = []
+        self.num_aces = 0
+        self.value = 0
+        self.dealer = dealer
+
+    def adjust_aces(self):
+        choice = input("You've received an Ace. Would you like to adjust its value? y/n > ").lower()
+        if choice == 'y':
+            print("Ace's value has been changed!")
+            self.num_aces -= 1
+            self.value -= 10
+        elif choice == 'n':
+            print("Ace stays the same!")
+        else:
+            print("Wrong input format, but whatever. Ace stays the same!")
+
+    def add_card(self, card):
+        self.deck.append(card)
+        self.value += values[card.rank]
+        if card.rank == "Ace" and not self.dealer:
+            self.num_aces += 1
+            self.adjust_aces()
+
+    def __str__(self):
+        hand_str = ''
+        for card in self.deck:
+            hand_str += card.__str__() + '\n'
+        return f"The player has the following cards:\n" + hand_str
+
+class Chips():
+
+    def __init__(self):
+        self.total = 100
+        self.bet = 0
+
+    def win_bet(self):
+        self.total += self.bet
+
+    def lose_bet(self):
+        self.total -= self.bet
